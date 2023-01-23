389c34af5b3053c7ad0bcf78f175dfba11a6314d
Fun Noober <71403430+FunNoober@users.noreply.github.com>
2023-01-23 16:04:25 -0500

    Improved Doors
    -Doors can now open in 2 different directions
    -Doors are now a state machine to determine their open state
    - Created a mirrored version of the door

--- ASSETS/Best of PD/scripts/objects/doors/Door.gd
+++ ASSETS/Best of PD/scripts/objects/doors/Door.gd
@@ -1,40 +1,78 @@
 extends Spatial
 
-var opened = false
-var peeked = false
+enum DOOR_STATES {
+	closed,
+	peek,
+	opened
+}
 
-export var open_angle = 90
-export var peek_angle = 15
-export var close_angle = 0
+enum OPEN_DIRECTIONS {
+	closed,
+	front,
+	back
+}
 
-func _on_PeekDoor_Door_Action(action_type) -> void:
-	if peeked == true and opened == false:
-		rotation_degrees.y = close_angle
-		peeked = false
-		opened = false
+var door_state = DOOR_STATES.closed
+var open_direction = OPEN_DIRECTIONS.closed
+
+export var peek_angle_front = 15
+export var open_angle_front = 90
+export var peek_angle_back = -15
+export var open_angle_back = -90
+export var start_angle = 90
+
+func _process(delta: float) -> void:
+	match door_state:
+		DOOR_STATES.closed:
+			rotation_degrees.y = lerp(rotation_degrees.y, start_angle, delta * 3)
+		DOOR_STATES.peek:
+			if open_direction == OPEN_DIRECTIONS.front:
+				rotation_degrees.y = lerp(rotation_degrees.y, peek_angle_front, delta * 3)
+			if open_direction == OPEN_DIRECTIONS.back:
+				rotation_degrees.y = lerp(rotation_degrees.y, peek_angle_back, delta * 3)
+		DOOR_STATES.opened:
+			if open_direction == OPEN_DIRECTIONS.front:
+				rotation_degrees.y = lerp(rotation_degrees.y, open_angle_front, delta * 3)
+			if open_direction == OPEN_DIRECTIONS.back:
+				rotation_degrees.y = lerp(rotation_degrees.y, open_angle_back, delta * 3)
+
+func peek(dir : int):
+	if door_state == DOOR_STATES.closed:
+		door_state = DOOR_STATES.peek
+		open_direction = dir
 		return
-	if peeked == false and opened == true:
-		rotation_degrees.y = peek_angle
-		peeked = true
-		opened = false
+	if door_state == DOOR_STATES.opened:
+		door_state = DOOR_STATES.peek
+		open_direction = dir
 		return
-	if peeked == false and opened == false:
-		rotation_degrees.y = peek_angle
-		peeked = true
+	if door_state == DOOR_STATES.peek:
+		door_state = DOOR_STATES.closed
+		open_direction = dir
 		return
 
-func _on_OpenDoor_Door_Action(action_type) -> void:
-	if peeked == true and opened == false:
-		rotation_degrees.y = open_angle
-		opened = true
-		peeked = false
+func open(dir : int):
+	if door_state == DOOR_STATES.closed:
+		door_state = DOOR_STATES.opened
+		open_direction = dir
 		return
-	if peeked == false and opened == true:
-		rotation_degrees.y = close_angle
-		opened = false
-		peeked = false
+	if door_state == DOOR_STATES.peek:
+		door_state = DOOR_STATES.opened
+		open_direction = dir
 		return
-	if peeked == false and opened == false:
-		rotation_degrees.y = open_angle
-		opened = true
+	if door_state == DOOR_STATES.opened:
+		door_state = DOOR_STATES.closed
+		open_direction = dir
 		return
+
+func _on_PeekDoorFront_Door_Action(action_type) -> void:
+	peek(OPEN_DIRECTIONS.front)
+		
+func _on_OpenDoorFront_Door_Action(action_type) -> void:
+	open(OPEN_DIRECTIONS.front)
+
+func _on_PeekDoorRear_Door_Action(action_type) -> void:
+	peek(OPEN_DIRECTIONS.back)
+	
+func _on_OpenDoorRear_Door_Action(action_type) -> void:
+	open(OPEN_DIRECTIONS.back)
+
